2244cad16e062b8e93b2d94fbdd299b769e35fae
Josh Wolfe <thejoshwolfe@gmail.com>
2014-11-25 00:51:23 -0700

    step 1: abstraction

--- a.js
+++ a.js
@@ -1,61 +1,48 @@
 
 var canvas = window.document.getElementById("canvas");
 var stepButton = window.document.getElementById("stepButton");
+var goButton = window.document.getElementById("goButton");
 
-var sizeX;
-var sizeY;
-var horizontalWalls;
-var verticalWalls;
+function MazeGenerator() {
+  this.sizeX = 0;
+  this.sizeY = 0;
+  this.horizontalWalls = null;
+  this.verticalWalls = null;
 
-var cellSize = 10;
-var cellSizeHalf = cellSize / 2;
-
-
-setSize(3, 3);
-render();
-function setSize(x, y) {
-  sizeX = x;
-  sizeY = y;
-  horizontalWalls = [];
+  this.cellSize = 10;
+  this.cellSizeHalf = this.cellSize / 2;
+}
+MazeGenerator.prototype.setSize = function(x, y) {
+  this.sizeX = x;
+  this.sizeY = y;
+  this.horizontalWalls = [];
   for (var i = 0; i < x; i++) {
     var ladder = [];
     for (var j = 0; j < y - 1; j++) {
       ladder.push(false);
     }
-    horizontalWalls.push(ladder);
+    this.horizontalWalls.push(ladder);
   }
-  verticalWalls = [];
+  this.verticalWalls = [];
   for (var i = 0; i < x - 1; i++) {
     var pole = [];
     for (var j = 0; j < y; j++) {
       pole.push(false);
     }
-    verticalWalls.push(pole);
+    this.verticalWalls.push(pole);
   }
-}
-
-stepButton.addEventListener("click", function() {
-  step();
-  render();
-});
-
-
-function step() {
-  var ladder = horizontalWalls[Math.floor(Math.random() * horizontalWalls.length)];
-  ladder[Math.floor(Math.random() * ladder.length)] = true;
-  var pole = verticalWalls[Math.floor(Math.random() * verticalWalls.length)];
-  pole[Math.floor(Math.random() * pole.length)] = true;
-}
-
-function render() {
+};
+MazeGenerator.prototype.render = function(canvas) {
   var context = canvas.getContext("2d");
+  var cellSize = this.cellSize;
+  var cellSizeHalf = this.cellSizeHalf;
   context.fillStyle = "#ffffff";
   context.fillRect(0, 0, 100, 100);
   context.strokeStyle = "#000000";
   // horizontalWalls
-  for (var i = 0; i < sizeX; i++) {
-    var ladder = horizontalWalls[i];
-    for (var j = -1; j < sizeY - 1 + 1; j++) {
+  for (var i = 0; i < this.sizeX; i++) {
+    var ladder = this.horizontalWalls[i];
+    for (var j = -1; j < this.sizeY - 1 + 1; j++) {
       var drawIt = ladder[j];
       if (drawIt == null || drawIt === true) {
         context.moveTo(i * cellSize + cellSize + cellSizeHalf, j * cellSize + cellSize + cellSizeHalf);
@@ -65,9 +52,9 @@ function render() {
     }
   }
   // verticalWalls
-  for (var i = -1; i < sizeX - 1 + 1; i++) {
-    var pole = verticalWalls[i];
-    for (var j = 0; j < sizeY; j++) {
+  for (var i = -1; i < this.sizeX - 1 + 1; i++) {
+    var pole = this.verticalWalls[i];
+    for (var j = 0; j < this.sizeY; j++) {
       var drawIt = pole == null || pole[j];
       if (drawIt === true) {
         context.moveTo(i * cellSize + cellSize + cellSizeHalf, j * cellSize + cellSize + cellSizeHalf);
@@ -76,4 +63,60 @@ function render() {
       }
     }
   }
+};
+
+inherits(KruskalGenerator, MazeGenerator);
+function KruskalGenerator() {
+  MazeGenerator.call(this);
+}
+KruskalGenerator.prototype.step = function() {
+  var ladder = this.horizontalWalls[Math.floor(Math.random() * this.horizontalWalls.length)];
+  ladder[Math.floor(Math.random() * ladder.length)] = true;
+  var pole = this.verticalWalls[Math.floor(Math.random() * this.verticalWalls.length)];
+  pole[Math.floor(Math.random() * pole.length)] = true;
+};
+
+var generator = new KruskalGenerator();
+
+generator.setSize(5, 5);
+generator.render(canvas);
+
+
+stepButton.addEventListener("click", function() {
+  step();
+});
+
+var animationInterval = null;
+goButton.addEventListener("click", function() {
+  if (animationInterval == null) {
+    // go
+    animationInterval = setInterval(function() {
+      step();
+    }, 100);
+    goButton.textContent = "Stop";
+  } else {
+    // stop
+    clearInterval(animationInterval);
+    animationInterval = null;
+    goButton.textcontent = "Go";
+  }
+});
+
+
+function step() {
+  generator.step();
+  generator.render(canvas);
+}
+
+function inherits(ctor, superCtor) {
+  // copied from nodejs's util.js
+  ctor.super_ = superCtor;
+  ctor.prototype = Object.create(superCtor.prototype, {
+    constructor: {
+      value: ctor,
+      enumerable: false,
+      writable: true,
+      configurable: true
+    }
+  });
 }
